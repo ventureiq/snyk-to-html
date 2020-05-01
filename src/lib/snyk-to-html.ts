d7080893da34c91f853a12960ec730044876fcd3
ekbsnyk <eleanor.kavanagh-brown@snyk.io>
2020-05-01 18:23:57 +0100

    fix: actionable remediations frontend
    refactored snyk-to-html to select remediations template if remediations arg passed in
    improved layout and styling of remediation report templates

--- src/lib/snyk-to-html.ts
+++ src/lib/snyk-to-html.ts
@@ -140,6 +140,7 @@ async function generateTemplate(data: any, template: string, remediation: boolea
   data.showSummaryOnly = summary;
 
   await registerPeerPartial(template, 'inline-css');
+  await registerPeerPartial(template, 'actionable-css');
   await registerPeerPartial(template, 'vuln-card');
   await registerPeerPartial(template, 'actionable-remediations');
 
